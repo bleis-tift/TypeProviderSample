717f52253da57ad57c7d156900a28e31b70e33a0
bleis-tift <bleis-tift@hotmail.co.jp>
2011-09-21 22:49:22 +0900

    型安全なRegexできた！
    日本に帰ってきて初めてのコミット

--- TP/CompiledType.fs
+++ TP/CompiledType.fs
@@ -13,7 +13,7 @@ let compile loadings moduleName src =
   let dll = System.IO.Path.GetTempFileName() + ".dll"
   let openMods = loadings |> List.map (sprintf "open %s\n") |> String.concat ""
   let src = "module " + moduleName + "\n" + openMods + src
-  let param = CompilerParameters(OutputAssembly=dll, CompilerOptions="--target:library")//(GenerateInMemory=true)
+  let param = CompilerParameters([| "System.dll" |], OutputAssembly=dll, CompilerOptions="--target:library")//(GenerateInMemory=true)
   let res = provider.CompileAssemblyFromSource(param, [| src |])
   let errors = res.Errors |> Seq.cast<CompilerError>
   if not(Seq.isEmpty errors) then
