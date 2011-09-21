32808d96d5dca7e7286ef725a21100a078dc7639
bleis-tift <bleis-tift@hotmail.co.jp>
2011-09-21 14:53:01 +0900

    型安全なsplitの作成に成功！
    ハワイからの帰りの飛行機の中にて。

---
+++ TP/CompiledType.fs
@@ -0,0 +1,23 @@
+﻿module CompiledType
+
+open Microsoft.FSharp.Compiler.CodeDom
+open System.Reflection
+open System.CodeDom.Compiler
+
+type t<'a> =
+| CompileError of CompilerError seq
+| Result of 'a
+
+let compile moduleName src =
+  use provider = new FSharpCodeProvider()
+  let dll = System.IO.Path.GetTempFileName() + ".dll"
+  let src = "module " + moduleName + "\nopen System\n" + src
+  let param = CompilerParameters(OutputAssembly=dll, CompilerOptions="--target:library")//(GenerateInMemory=true)
+  let res = provider.CompileAssemblyFromSource(param, [| src |])
+  let errors = res.Errors |> Seq.cast<CompilerError>
+  if not(Seq.isEmpty errors) then
+    CompileError errors
+  else
+    let asm = res.CompiledAssembly
+    let t = asm.GetType(moduleName)
+    Result t
---
+++ TP/TypeSafeSplit.fs
@@ -0,0 +1,52 @@
+﻿namespace StringUtil
+
+open System
+open System.Collections.Generic
+open System.Linq.Expressions
+open System.Globalization
+open System.Reflection
+open System.Diagnostics
+open Microsoft.FSharp.Core.CompilerServices
+open System.Text.RegularExpressions
+
+[<assembly: TypeProviderAssembly>]
+do()
+
+type Splitter = class end
+
+[<TypeProvider>]
+type SplitterProvider() =
+  let invalidation = Event<EventHandler, EventArgs>()
+  interface IProvidedNamespace with
+    member this.ResolveTypeName(typeName) = typeof<Splitter>
+    member this.NamespaceName with get() = "StringUtil"
+    member this.GetNestedNamespaces() = Array.empty
+    member this.GetTypes() = [| typeof<Splitter> |]
+  interface ITypeProvider with
+    member this.GetNamespaces() = [| this |]
+    member this.Dispose() = ()
+    [<CLIEvent>]
+    member this.Invalidate = invalidation.Publish
+    member this.GetStaticParameters(typeWithoutArguments) =
+      [| { new ParameterInfo() with
+             member x.Name = "count"
+             member x.ParameterType = typeof<int> } |]
+    member this.ApplyStaticArguments(typeWithoutArgs, typeNameWithArgs, staticArgs) =
+      let makeSplit name count =
+        let pat = List.init count (fun i -> "s" + (string i)) |> String.concat "; "
+        let tpl = List.init count (fun i -> "s" + (string i)) |> String.concat ", "
+        let f = "let split =\n" +
+                "  let splitImpl (sep: string) (str: string) =\n" +
+                "    match str.Split([| sep |], " + (string count) + ", StringSplitOptions.None) with\n" +
+                "    | [| " + pat + " |] -> (" + tpl + ")\n" +
+                "    | _ -> failwith \"not match.\"\n" +
+                "  splitImpl"
+        match CompiledType.compile name f with
+        | CompiledType.Result t -> t
+        | CompiledType.CompileError e -> failwith "oops!"
+      let r = makeSplit typeNameWithArgs (staticArgs.[0] :?> int)
+      r
+    member this.GetInvokerExpression(syntheticMethodBase, parameters) =
+      let m = syntheticMethodBase :?> MethodInfo
+      let args = parameters |> Seq.cast<Expression>
+      Expression.Call(null, m, args) :> Expression
